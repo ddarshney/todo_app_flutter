2495f81b1731c1026aed7a6b19f535034031c07d
ddarshney <darshneyd@gmail.com>
2021-05-22 23:39:12 +0530

    :sparkles: changed background of homepage

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -24,7 +24,7 @@ class HomePage extends StatelessWidget {
         decoration: BoxDecoration(
           image: DecorationImage(
               fit: BoxFit.fill,
-              image: AssetImage('assets/images/background2.jpg')),
+              image: AssetImage('assets/images/background3.png')),
         ),
         child: BackdropFilter(
           filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
